b21b4c30966660319e69b96a99d4c3a8dcad0c29
Kevin Bai <lkevin957@gmail.com>
2019-03-22 15:15:55 -0700

    Finish is heap

--- Graphs & Heaps/heaps_project/lib/is_heap.js
+++ Graphs & Heaps/heaps_project/lib/is_heap.js
@@ -1,6 +1,22 @@
 // you may assume that the array will always have a null element at the 0-th index
 function isMaxHeap(array, idx=1) {
+    let root;
+    let left;
+    let right;
+    for (let index = 1; index < array.length; index++) {
+        root = array[index];
+        left = array[index * 2];
+        right = array[index * 2 + 1];
 
+        if ( root < left ){
+            return false;
+        }
+        if ( root < right ){
+            return false;
+        }
+    }
+
+    return true;
 }
 
 module.exports = {
