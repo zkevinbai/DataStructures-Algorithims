72309b7ad5a652fecbbe890c3d294d62e9b28915
Kevin Bai <lkevin957@gmail.com>
2019-03-22 09:56:22 -0700

    BFS search

--- Graphs & Heaps/graph_project/lib/breadth_first_search.js
+++ Graphs & Heaps/graph_project/lib/breadth_first_search.js
@@ -1,5 +1,34 @@
 function breadthFirstSearch(startingNode, targetVal) {
+    let q = [];
+    let obj = {};
+    if (startingNode.val === targetVal) {
+        return startingNode;
+    } else if (startingNode.val !== targetVal && !startingNode.neighbors.length) {
+        return null;
+    }
+    let node = startingNode;
+    obj[JSON.stringify(node)] = true;
+    // obj[node] = true;
+    q = q.concat(node.neighbors);
 
+    while (q.length) {
+        for (let i = 0; i < q.length; i++) {
+            node = q.shift();
+            if (obj[JSON.stringify(node)] === true) {
+            // if (obj[node] === true) {
+                return null;
+            }
+
+            if (node.val === targetVal){
+                return node;
+            } else if (node.neighbors) {
+                q = q.concat(node.neighbors);
+            } 
+            obj[JSON.stringify(node)] = true;
+            // obj[node] = true;
+        }
+    }
+    return null; 
 }
 
 module.exports = {
