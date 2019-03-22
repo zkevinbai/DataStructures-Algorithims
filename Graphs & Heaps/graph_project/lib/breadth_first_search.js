79cb2c3e24592696f87f9e2ed7659d5af8e4f246
Kevin Bai <lkevin957@gmail.com>
2019-03-22 10:08:05 -0700

    Finish BFS for graph

--- Graphs & Heaps/graph_project/lib/breadth_first_search.js
+++ Graphs & Heaps/graph_project/lib/breadth_first_search.js
@@ -1,21 +1,23 @@
 function breadthFirstSearch(startingNode, targetVal) {
     let q = [];
     let obj = {};
+    let circleKiller = [];
+    
     if (startingNode.val === targetVal) {
         return startingNode;
     } else if (startingNode.val !== targetVal && !startingNode.neighbors.length) {
         return null;
     }
     let node = startingNode;
-    obj[JSON.stringify(node)] = true;
-    // obj[node] = true;
+    obj[node.val + 0] = true;
     q = q.concat(node.neighbors);
 
     while (q.length) {
         for (let i = 0; i < q.length; i++) {
             node = q.shift();
-            if (obj[JSON.stringify(node)] === true) {
-            // if (obj[node] === true) {
+
+            //Check for cycle 
+            if (circleKiller.includes(node)) {
                 return null;
             }
 
@@ -24,12 +26,14 @@ function breadthFirstSearch(startingNode, targetVal) {
             } else if (node.neighbors) {
                 q = q.concat(node.neighbors);
             } 
-            obj[JSON.stringify(node)] = true;
-            // obj[node] = true;
+
+            circleKiller.push(node);
         }
     }
     return null; 
 }
+
+
 
 module.exports = {
     breadthFirstSearch
