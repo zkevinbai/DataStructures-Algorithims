711c49288829d87436f43668bf9ef33a8019ccd1
Kevin Bai <lkevin957@gmail.com>
2019-03-25 11:42:09 -0700

    attempt largest matrix again

--- Algorithim Test/4-caseysImageEditing.js
+++ Algorithim Test/4-caseysImageEditing.js
@@ -1,7 +1,27 @@
 function largestMatrix(arr) {
-    // Write your code here
-    let flat = arr.flat();
-    if (!flat.includes("0")){
-        return arr.length - 1;
+    let squared = arr.length ** 2;
+    let zeroCount = 0;
+    let max = arr.length - 1;
+
+    for (let index = 0; index < arr.length; index++) {
+        let subArr = arr[index];
+
+
+        for (let index = 0; index < subArr.length; index++) {
+            if (subArr[index] === 0){
+                max -= 1;
+                zeroCount += 1;
+            }            
+        }        
     }
+
+    if (zeroCount === squared) return 0;
+    if ( max < 1) return 1;
+
+    return max;
 }
+let arr = [
+[0, 1, 1],
+[1, 1, 0],
+[1, 0, 1]
+];
